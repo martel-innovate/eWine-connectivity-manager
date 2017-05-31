f6e4a2aaab9c45ce35cd9d1ec9f1da3b4a106b41
Michele Chersich <michele.chersich@gmail.com>
2017-05-31 19:53:08 +0200

    wifi_manager.py: added route 'network_delete_all' python_manager.sh: moved to new folder 'interpreter'

--- python_wifi.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd /home/pi/eWine
-sudo python wifi_connect.py wlan0 Telecom-86623587 -p Messedanime87!
-#sudo python wifi_connect.py wlan0 Sunrise_2.4GHz_6469F0 -p 9h9q5yc2Ynbb
-#sudo python wifi_connect.py wlan0 AndroidAP -p iohe2982
-#sudo python wifi_connect.py wlan0 Sunrise_2.4GHz_6469F0 -d
