26f721d8ed772c114f269e0d9e64e2ed92145784
Michele Chersich <michele.chersich@gmail.com>
2017-06-29 13:56:41 +0200

    added test unit

---
+++ wifi_manager/tests/test_wifi.py
@@ -0,0 +1,22 @@
+import os
+import wifi_rest
+import unittest
+import tempfile
+
+
+class WifiTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, wifi_rest.app.config['DATABASE'] = tempfile.mkstemp()
+        wifi_rest.app.testing = True
+        self.app = wifi_rest.app.test_client()
+        with wifi_rest.app.app_context():
+            wifi_rest.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(wifi_rest.app.config['DATABASE'])
+
+
+if __name__ == '__main__':
+    unittest.main()
