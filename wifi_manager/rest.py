a6494a52652f9e7b3b781f6933a4c7b275071671
chersicm <michele.chersich@gmail.com>
2017-09-07 13:25:26 +0200

    renamed script files

--- wifi_manager/rest.py
+++ wifi_manager/rest.py
@@ -1,9 +1,6 @@
-from binascii import hexlify
 from functools import wraps
 from flask import Flask, request, g, jsonify
-from wifi_core import *
-
-import os
+from core import *
 
 app = Flask(__name__)
 
@@ -18,7 +15,7 @@ def require_api_key(route_function):
 
     @wraps(route_function)
     def check_api_key(*args, **kwargs):
-        if request.headers.get('X-api-key') and request.headers.get('X-api-key') == API_KEY:
+        if request.headers.get('X-api-key') and request.headers.get('X-api-key') == app.API_KEY:
             return route_function(*args, **kwargs)
         else:
             return jsonify(message='unauthorized: wrong or missing api key', code=401)
@@ -41,7 +38,7 @@ def _get_db():
     return db
 
 
-def _init_db():
+def init_db():
     """
     initialize database from sqlite3 script file
     
@@ -253,17 +250,3 @@ def network_delete_all():
     total, deleted = wifi_delete_all(_get_db())
 
     return jsonify(message='deleted {}/{} schemes'.format(total, deleted), code=200)
-
-
-if __name__ == '__main__':
-    API_KEY = hexlify(os.urandom(20)).decode()
-    print('api key is: {}'.format(API_KEY))
-
-    app.config['DB_PATH'] = '/home/pi/eWine-connectivity-manager/wifi_manager/schema'
-    app.config['DB_SOURCE'] = os.path.join(app.config['DB_PATH'], 'schema.sql')
-    app.config['DB_INSTANCE'] = os.path.join(app.config['DB_PATH'], 'schema.db')
-
-    app.config['DEBUG'] = True
-
-    _init_db()
-    app.run(host='0.0.0.0')
