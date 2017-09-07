e6255a17ba49f788905680d7ad5d55e277615253
chersicm <michele.chersich@gmail.com>
2017-09-07 14:25:37 +0200

    added unit tests in test_core.py, interpreter bash script to run as sudo and context.py to import package modules

---
+++ tests/context.py
@@ -0,0 +1,8 @@
+
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import wifi_manager.core as core
+import wifi_manager.rest as rest
---
+++ tests/interpreter/python_wifi_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/pi/eWine-connectivity-manager/tests
+sudo python test_core.py
