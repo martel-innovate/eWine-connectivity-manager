12d193051399c1a891c84c1876a067c6a5b1355c
chersicm <michele.chersich@gmail.com>
2017-09-11 18:43:22 +0200

    completed unit tests first version

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,48 +5,139 @@ import tempfile
 
 
 class WifiCoreTestCase(unittest.TestCase):
+    """
+    Before running the tests, perform the following setup operations:
+    - enable 'wlan0'
+    - connect to a network
+    """
 
     def setUp(self):
-        self.iface = 'wlan0'
-
-        rest.app.config['DB_PATH'] = '/home/pi/eWine-connectivity-manager/wifi_manager/schema'
+        dir_name = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        rest.app.config['DB_PATH'] = os.path.join(dir_name, 'wifi_manager/schema')
         rest.app.config['DB_SOURCE'] = os.path.join(rest.app.config['DB_PATH'], 'schema.sql')
         self.db_fd, rest.app.config['DB_INSTANCE'] = tempfile.mkstemp()
-
         rest.app.testing = True
-        self.app = rest.app.test_client()
 
         with rest.app.app_context():
             rest.init_db()
+            self.db_ro = rest.get_db()
 
-    def test_1_enable(self):
-        code = core.enable(self.iface)
-        self.assertEqual(code, 0)
-
-        ifaces = core.interfaces()
-        self.assertNotIn(self.iface, ifaces)
-
-    def test_2_connect(self):
-        optimal = core.optimal(self.iface)
-        self.assertTrue(len(optimal) > 0)
-
-        core.connect(self.iface, optimal, None, self.db_fd)
-
-        ifaces = core.interfaces()
-        self.assertIn(self.iface, ifaces)
-
-    def test_3_disable(self):
-        code = core.disable(self.iface)
-        self.assertEqual(code, 0)
-
-        ifaces = core.interfaces()
-        self.assertNotIn(self.iface, ifaces)
+        self.iface = 'wlan0'
 
     def tearDown(self):
         os.close(self.db_fd)
         os.unlink(rest.app.config['DB_INSTANCE'])
 
+    def test_0a_disable(self):
+        code = core.disable(self.iface)
+        self.assertEqual(code, 0)
+
+    def test_0b_interfaces(self):
+        ifaces = core.interfaces()
+        self.assertIsInstance(ifaces, list)
+        self.assertNotIn(self.iface, ifaces)
+
+    def test_0c_status(self):
+        status = core.status(self.iface)
+        self.assertEqual(status, '')
+
+    def test_0d_scan(self):
+        self.assertRaises(core.WifiException, core.cell_all, self.iface)
+
+    def test_0e_optimal(self):
+        self.assertRaises(core.WifiException, core.optimal, self.iface)
+
+    def test_0f_save(self):
+        self.assertRaises(core.WifiException, core.save, self.iface, 'foo', 'foo', self.db_ro)
+
+    def test_0g_connect(self):
+        self.assertRaises(core.WifiException, core.connect, self.iface, 'foo', 'foo', self.db_ro)
+
+    def test_1a_enable(self):
+        code = core.enable(self.iface)
+        self.assertEqual(code, 0)
+
+    def test_1b_interfaces(self):
+        ifaces = core.interfaces()
+        self.assertIsInstance(ifaces, list)
+        self.assertNotIn(self.iface, ifaces)
+
+    def test_1c_status(self):
+        status = core.status(self.iface)
+        self.assertEqual(status, '')
+
+    def test_1d_scan(self):
+        cells = core.cell_all(self.iface)
+        self.assertIsInstance(cells, list)
+
+        for c in cells:
+            self.assertIsInstance(c["ssid"], unicode)
+            self.assertIsInstance(c["signal"], int)
+            self.assertIsInstance(c["quality"], unicode)
+            self.assertIsInstance(c["frequency"], unicode)
+            self.assertIsInstance(c["bitrates"], list)
+            self.assertIsInstance(c["encrypted"], bool)
+            self.assertIsInstance(c["channel"], int)
+            self.assertIsInstance(c["address"], unicode)
+            self.assertIsInstance(c["mode"], unicode)
+
+            if c["encrypted"]:
+                self.assertIsInstance(c["encryption_type"], str)
+
+    def optimal_test(self):
+        # the optimal network configuration must be stored before running the test
+        optimal = core.optimal(self.iface)
+        self.assertIsInstance(optimal, str)
+        self.assertTrue(len(optimal) > 0)
+        return optimal
+
+    def test_1e_save(self):
+        self.assertRaises(core.WifiException, core.save, self.iface, 'foo', 'foo', self.db_ro)
+
+    def test_2a_connect(self):
+        optimal = self.optimal_test()
+        with rest.app.app_context():
+            core.connect(self.iface, optimal, None, rest.get_db())
+
+    def test_2b_interfaces(self):
+        ifaces = core.interfaces()
+        self.assertIsInstance(ifaces, list)
+        self.assertIn(self.iface, ifaces)
+
+    def test_2c_status(self):
+        status = core.status(self.iface)
+        optimal = self.optimal_test()
+        self.assertIsInstance(status, str)
+        self.assertEqual(status, optimal)
+
+    def test_3a_delete_all(self):
+        with rest.app.app_context():
+            total, deleted = core.delete_all(rest.get_db())
+        self.assertEquals(total, deleted)
+
+    def test_3b_disable(self):
+        code = core.disable(self.iface)
+        self.assertEqual(code, 0)
+
+    def test_scheme_all(self):
+        schemes = core.scheme_all()
+        for obj in schemes:
+            iface, name = obj['interface'], obj['name']
+            self.assertIsInstance(iface, str)
+            self.assertIsInstance(name, str)
+            self.assertTrue(len(iface) > 0)
+            self.assertTrue(len(name) > 0)
+
+            options = obj['options']
+            self.assertIsInstance(options, dict)
+            channel, psk, ssid = options['wireless-channel'], options['wpa-psk'], options['wpa-ssid']
+            self.assertIsInstance(channel, str)
+            self.assertIsInstance(psk, str)
+            self.assertIsInstance(ssid, str)
+            self.assertTrue(len(channel) > 0)
+            self.assertTrue(len(psk) > 0)
+            self.assertTrue(len(ssid) > 0)
+
 
 if __name__ == '__main__':
     unittest.main()
-
--- tests/test_rest.py
+++ tests/test_rest.py
@@ -1,3 +1,6 @@
+from binascii import hexlify
+
+from flask import json
 from context import rest
 
 import os
@@ -6,17 +9,177 @@ import tempfile
 
 
 class WifiRestTestCase(unittest.TestCase):
+    """
+        Before running the tests, perform the following setup operations:
+        - enable 'wlan0'
+        - connect to a network
+        """
 
     def setUp(self):
+        dir_name = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        rest.app.config['DB_PATH'] = os.path.join(dir_name, 'wifi_manager/schema')
+        rest.app.config['DB_SOURCE'] = os.path.join(rest.app.config['DB_PATH'], 'schema.sql')
         self.db_fd, rest.app.config['DB_INSTANCE'] = tempfile.mkstemp()
+
         rest.app.testing = True
         self.app = rest.app.test_client()
+
         with rest.app.app_context():
             rest.init_db()
+
+        rest.app.API_KEY = hexlify(os.urandom(20)).decode()
+
+        self.iface = 'wlan0'
+        self.cell = 'foo'
+        self.cellnotfound = "cell {}: not found".format(self.cell)
+        self.netdown = self.iface + "     Interface doesn't support scanning : Network is down"
+        self.netsaved = "created {}:{}"
+        self.netconnected = "connected {}:{}"
+        self.netalldeleted = "deleted {}/{} schemes"
+        self.netnotfound = "scheme {}: not found"
 
     def tearDown(self):
         os.close(self.db_fd)
         os.unlink(rest.app.config['DB_INSTANCE'])
+
+    def test_0a_disable(self):
+        resp = self.app.post('/disable/' + self.iface, headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 200)
+        self.assertEquals(resp_dict['message'], 'disabled {}'.format(self.iface))
+
+    def test_0b_scan(self):
+        resp = self.app.get('/scan/' + self.iface, headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 404)
+        self.assertEquals(resp_dict['message'], self.netdown)
+
+    def test_0c_optimal(self):
+        resp = self.app.get('/optimal/' + self.iface, headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 404)
+        self.assertEquals(resp_dict['message'], self.netdown)
+
+    def test_0d_save(self):
+        resp = self.app.post('/networks/' + self.iface + ':foo', headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 404)
+        self.assertEquals(resp_dict['message'], self.netdown)
+
+    def test_0e_connect(self):
+        resp = self.app.post('/connect/' + self.iface + ':foo', headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 404)
+        self.assertEquals(resp_dict['message'], self.netdown)
+
+    def test_1a_enable(self):
+        resp = self.app.post('/enable/' + self.iface, headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 200)
+        self.assertEquals(resp_dict['message'], 'enabled {}'.format(self.iface))
+
+    def test_1b_scan(self):
+        resp = self.app.get('/scan/' + self.iface, headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 200)
+        self.assertIsInstance(resp_dict['message'], list)
+
+    def optimal_test(self):
+        resp = self.app.get('/optimal/' + self.iface, headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 200)
+        self.assertIsInstance(resp_dict['message'], unicode)
+        self.assertTrue(len(resp_dict['message']) > 0)
+        return resp_dict['message']
+
+    def test_1d_save(self):
+        optimal = self.optimal_test()
+        resp = self.app.post('/networks/' + self.iface + ':' + optimal, headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 201)
+        self.assertEquals(resp_dict['message'], self.netsaved.format(self.iface, optimal))
+
+    def test_1e_connect(self):
+        optimal = self.optimal_test()
+        resp = self.app.post('/connect/' + self.iface + ':' + optimal, headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 200)
+        self.assertEquals(resp_dict['message'], self.netconnected.format(self.iface, optimal))
+
+    def stored_networks(self):
+        resp = self.app.get('/networks', headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 200)
+        self.assertIsInstance(resp_dict['message'], list)
+        return len(resp_dict['message'])
+
+    def test_2a_delete_all(self):
+        n = self.stored_networks()
+        resp = self.app.delete('/networks', headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 200)
+        self.assertEquals(resp_dict['message'], self.netalldeleted.format(n, n))
+
+    def test_2b_delete(self):
+        resp = self.app.delete('/networks/' + self.iface + ':foo', headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 404)
+        self.assertEquals(resp_dict['message'], self.netnotfound.format('foo'))
+
+    def test_2c_disable(self):
+        resp = self.app.post('/disable/' + self.iface, headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 200)
+        self.assertEquals(resp_dict['message'], 'disabled {}'.format(self.iface))
+
+    def single_test_api_key(self, route, method):
+        if method == 'GET':
+            resp = self.app.get(route)
+        elif method == 'POST':
+            resp = self.app.post(route)
+        elif method == 'DELETE':
+            resp = self.app.delete(route)
+
+        resp_dict = json.loads(resp.get_data())
+
+        self.assertEqual(resp_dict['message'], 'unauthorized: wrong or missing api key')
+        self.assertEqual(resp_dict['code'], 401)
+
+    def test_all_api_key(self):
+        self.single_test_api_key('/networks', 'GET')
+        self.single_test_api_key('/ifaces', 'GET')
+        self.single_test_api_key('/scan/' + self.iface, 'GET')
+        self.single_test_api_key('/status/' + self.iface, 'GET')
+        self.single_test_api_key('/enable/' + self.iface, 'POST')
+        self.single_test_api_key('/disable/' + self.iface, 'POST')
+        self.single_test_api_key('/networks/iface:ssid', 'POST')
+        self.single_test_api_key('/optimal/' + self.iface, 'GET')
+        self.single_test_api_key('/connect/iface:ssid', 'POST')
+        self.single_test_api_key('/networks/iface:ssid', 'DELETE')
+        self.single_test_api_key('/networks', 'DELETE')
+
+    def test_networks(self):
+        resp = self.app.get('/networks', headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 200)
+        self.assertIsInstance(resp_dict['message'], list)
+
+    def test_ifaces(self):
+        resp1 = self.app.get('/ifaces', headers={'X-Api-Key': rest.app.API_KEY})
+        resp1_dict = json.loads(resp1.get_data())
+        self.assertEquals(resp1_dict['code'], 200)
+        self.assertIsInstance(resp1_dict['message'], list)
+
+        resp2 = self.app.get('/ifaces/addr', headers={'X-Api-Key': rest.app.API_KEY})
+        resp2_dict = json.loads(resp2.get_data())
+        self.assertEquals(resp2_dict['code'], 200)
+        self.assertIsInstance(resp2_dict['message'], list)
+
+    def test_status(self):
+        resp = self.app.get('/status/' + self.iface, headers={'X-Api-Key': rest.app.API_KEY})
+        resp_dict = json.loads(resp.get_data())
+        self.assertEquals(resp_dict['code'], 200)
+        self.assertIsInstance(resp_dict['message'], unicode)
 
 
 if __name__ == '__main__':
